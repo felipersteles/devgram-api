14f434d8347a7a4c7e93acfb9cdd89c81bdedbe0
Felipe Teles <felipersteles@gmail.com>
2022-09-27 11:47:31 -0300

    jwt commit

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,24 +2,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { conectMongoDB } from '../../middleware/conectMongoDB';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg.type';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
+import { LoginResposta } from '../../types/LoginResposta.type';
 
-const endpointLogin = (
+const endpointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+
+    const { CHAVE_JWT } = process.env;
+    if(!CHAVE_JWT) return res.status(500).json({erro: 'Chave JWT não informada >:('})
+
     //login é um post
     if (req.method === 'POST') {
         const { login, senha } = req.body
         
-        if (login === 'exemplo@admin.com' && senha === 'exemplo123') {
-            return res.status(200).json({msg: 'Usuário autenticado com sucesso :)'})
+        const usuariosEncontrados = await UsuarioModel.find({ email: login, senha: md5(senha) });
+        if (usuariosEncontrados && usuariosEncontrados.length > 0) {
+            const usuarioEncontrado = usuariosEncontrados[0];
+
+            const token = jwt.sign({_id: usuarioEncontrado._id}, CHAVE_JWT)
+
+            return res.status(200).json({
+                nome: usuarioEncontrado.nome,
+                email: usuarioEncontrado.email,
+                token: token
+            });
         }
 
-        return res.status(405).json({erro: 'Usuário ou senha não encontrado!'});
+        return res.status(400).json({erro: 'Usuário ou senha não encontrado!'});
     }
 
     //console.log(req.method)
-    return res.status(405).json({erro: 'Método informado não é válido :/'})
+    return res.status(405).json({erro: 'Método inexistente :/'})
 }
 
 //primeiro passa no middleware dps no endpoint
---
+++ types/LoginResposta.type.ts
@@ -0,0 +1,8 @@
+
+export type LoginResposta = {
+    nome: string,
+    email: string,
+    token: string
+}
+
+
